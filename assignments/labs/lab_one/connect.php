8e4658539b513636fda004dc2d44abc809cad08f
Tyson Ward <tysonward109@gmail.com>
2026-01-23 13:22:53 -0500

    week three fork

--- assignments/labs/lab_one/connect.php
+++ assignments/labs/lab_one/connect.php
@@ -1,11 +1,11 @@
 <?php
 declare(strict_types=1);
-//Database connection settings 
+//Database connection settings in a variable 
 $host = "localhost";
 $db = "lab_one";
 $user = "root";
 $password = "";
-
+//Data source name, type of database, location and database name
 $dsn = "mysql:host=$host;dbname=$db";
 
 try {
@@ -13,7 +13,9 @@ try {
     $pdo = new PDO($dsn, $user, $password);
     //Set the PDO error mode to exception 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "<p>Connected successfully</p>";
 } catch (PDOException $e) 
-{   //If connection fails display error message
+{   die ("Database connection failed: " . $e->getMessage());
+    //If connection fails display error message
     echo "Connection failed: " . $e->getMessage();
 }
