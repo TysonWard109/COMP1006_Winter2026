e86d3de8833a600a9c54f95667e6f876ff76ce1d
Tyson Ward <tysonward109@gmail.com>
2026-03-20 20:43:28 -0400

    Lesson 10 completed

--- 10/add-product.php
+++ 10/add-product.php
@@ -41,6 +41,47 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 
     //Add Code Here 
+    //Check if a file was uploaded
+
+    if(isset($_FILES['product_image']) && $_FILES['product_image']['error'] !== UPLOAD_ERR_NO_FILE){
+        // make sure upload completed succesfully 
+        if($_FILES['product_image']['error'] !==UPLOAD_ERR_OK){
+            $errors[] = "There was a problem uploading your file";
+        }
+        else{
+            // Bulding array to only allow a few files types
+            $allowedTypes = ['image/jpeg', 'image/png', 'image/webp', 'image/jpg'];
+
+            //Detect the real MIME type of file
+            $detectedType = mime_content_type($_FILES['product_image']['tmp_name']);
+
+            if(!in_array($detectedType, $allowedTypes, true)){
+                $errors[] = "Only JPG, PNG and WebP allowed";
+            }
+            else{
+                //build the file name and move it to where we want it to (uploads)
+                // get the file extension 
+                $extension = pathinfo($_FILES['product_image']['name'], PATHINFO_EXTENSION);
+
+                //Create unique file name so the uploaded files dont overwrite
+                $safeFilename = uniqid('product_', true ). '.' . strtolower($extension);
+
+                // Build the full server path where the file will be store
+                $destination = __DIR__ .'/uploads/'. $safeFilename;
+
+                if(move_uploaded_file($_FILES['product_image']['tmp_name'], $destination)){
+                    //Save the relative path to the database
+                    $imagePath = 'uploads/' . $safeFilename;
+                }
+                else{
+                    $errors[]= "Image upload failed";
+                }
+            }
+        }
+
+    }
+
+
 
     // If there are no errors, insert the product into the database
     if (empty($errors)) {
